8a90dcd9d51f5090a16a8e70c5e449e9a9fba03f
unknown <bouchraelaili247@gmail.com>
2021-04-12 10:05:43 +0000

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <ParcourList title="Formation"/>
+    <ParcourInput/>
+    <ParcourList title="Experience"/>
+    <ParcourInput/>
+    <ParcourList title="technologie"/>
+    <ParcourInput/>
+  </div>
+</template>
+
+<script>
+import ParcourList from '@/components/ParcourList'
+import ParcourInput from '../components/ParcourInput.vue'
+
+export default {
+  name: "Home",
+  components: {
+    ParcourList,
+    ParcourInput,
+  }
+}
+</script>
